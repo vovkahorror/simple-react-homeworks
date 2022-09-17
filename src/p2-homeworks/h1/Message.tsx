d73cc244ea280be2c30ead106d53630f94a06ad6
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-09-17 19:55:30 +0300

    started the first task

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Message() {
+    return (
+        <div>
+{   avatar={messageData.avatar}}
+            {   name={messageData.name}}
+            {   message={messageData.message}}
+            {   time={messageData.time}}
+        </div>
+    )
+}
+
+export default Message
